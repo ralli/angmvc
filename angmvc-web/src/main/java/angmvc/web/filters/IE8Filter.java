03756fc0055c45e6c82068b6fbb8e8ee6a0150f8
Ralph Juhnke <r_juhnke@gmx.de>
2014-01-18 01:56:11 +0100

    Added IE and NoCache Filters

---
+++ angmvc-web/src/main/java/angmvc/web/filters/IE8Filter.java
@@ -0,0 +1,37 @@
+package angmvc.web.filters;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class IE8Filter implements Filter {
+  public static final Logger log = LoggerFactory.getLogger(IE8Filter.class);
+
+  /**
+   * Empty implementation of the Filter-Interface
+   *
+   * @param filterConfig the Filterconfig
+   * @throws ServletException
+   */
+  @Override
+  public void init(FilterConfig filterConfig) throws ServletException {
+  }
+
+  @Override
+  public void doFilter(ServletRequest request, ServletResponse response,
+                       FilterChain chain) throws IOException, ServletException {
+    HttpServletResponse res = (HttpServletResponse) response;
+    res.addHeader("X-UA-Compatible", "IE=edge");
+    chain.doFilter(request, response);
+  }
+
+  /**
+   * Empty implementation of the Filter-Interface
+   */
+  @Override
+  public void destroy() {
+  }
+}
---
+++ angmvc-web/src/main/java/angmvc/web/filters/NoCacheFilter.java
@@ -0,0 +1,38 @@
+package angmvc.web.filters;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class NoCacheFilter implements Filter {
+  public static final Logger log = LoggerFactory.getLogger(NoCacheFilter.class);
+
+  /**
+   * Empty implementation of the Filter-Interface
+   *
+   * @param filterConfig the Filterconfig
+   * @throws javax.servlet.ServletException
+   */
+  @Override
+  public void init(FilterConfig filterConfig) throws ServletException {
+  }
+
+  @Override
+  public void doFilter(ServletRequest request, ServletResponse response,
+                       FilterChain chain) throws IOException, ServletException {
+    HttpServletResponse res = (HttpServletResponse) response;
+    res.addHeader("Pragma", "no-cache");
+    res.addHeader("Cache-Control", "no-cache, no-store");
+    chain.doFilter(request, response);
+  }
+
+  /**
+   * Empty implementation of the Filter-Interface
+   */
+  @Override
+  public void destroy() {
+  }
+}
