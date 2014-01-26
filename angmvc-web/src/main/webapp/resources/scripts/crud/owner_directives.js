7cc793797521814ba3c06f6252d16db7be6ca355
Ralph Juhnke <r_juhnke@gmx.de>
2014-01-26 12:11:56 +0100

    Delete Link

--- angmvc-web/src/main/webapp/resources/scripts/crud/owner_directives.js
+++ angmvc-web/src/main/webapp/resources/scripts/crud/owner_directives.js
@@ -17,13 +17,30 @@
     return {
       'restrict': 'EA',
       'scope': {
-        'deleteHref': '@'
+        'deleteHref': '@',
+        'success': '=',
+        'error': '='
       },
       'transclude': true,
       'template': "<a ng-click=\"clicked();\" ng-transclude></a>",
       'link': function(scope) {
         scope.clicked = function() {
-          $http({'method': 'POST', url: scope.deleteHref});
+          var successCallback = function(data, status, headers, config) {
+            if(angular.isDefined(scope.success)) {
+              scope.success(data, status, headers, config);
+            }
+          };
+
+          var errorCallback = function(data, status, headers, config) {
+            if(angular.isDefined(scope.error)) {
+              scope.error(data, status, headers, config);
+            }
+          };
+
+          $http({'method': 'POST', url: scope.deleteHref})
+            .success(successCallback)
+            .error(errorCallback);
+
           return false;
         }
       }
--- angmvc-web/src/main/webapp/resources/scripts/crud/owner_list_ctrl.js
+++ angmvc-web/src/main/webapp/resources/scripts/crud/owner_list_ctrl.js
@@ -1,14 +1,28 @@
 'use strict';
 
 (function () {
-    var app = angular.module("ownerApp");
+  var app = angular.module("ownerApp");
 
-    app.controller('OwnerListCtrl', ["$scope", "ownerService", function($scope, ownerService) {
-        ownerService.query({},
-            function(data) {
-                $scope.owners = data;
-            },
-            function(error) {});
-    }]);
+  app.controller('OwnerListCtrl', ["$scope", "ownerService", "messageService", function ($scope, ownerService, messageService) {
+    $scope.deleteSuccess = function (data, status, headers, config) {
+      $scope.reload();
+      messageService.showSuccess("Successfully deleted owner");
+    };
+
+    $scope.deleteError = function (data, status, headers, config) {
+      messageService.showError("Error deleting owner");
+    };
+
+    $scope.reload = function () {
+      ownerService.query({},
+        function (data) {
+          $scope.owners = data;
+        },
+        function (error) {
+        });
+    }
+
+    $scope.reload();
+  }]);
 
 }).call(this);
